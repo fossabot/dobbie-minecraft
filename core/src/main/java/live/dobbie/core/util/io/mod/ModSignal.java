a19c83fdaafa32f9a821ede8caef3fa83ebab387
j0x0f <55847520+j0x0f@users.noreply.github.com>
2020-01-11 02:39:19 +0500

    fix javadoc warning

--- core/src/main/java/live/dobbie/core/util/io/mod/ModSignal.java
+++ core/src/main/java/live/dobbie/core/util/io/mod/ModSignal.java
@@ -6,7 +6,7 @@ package live.dobbie.core.util.io.mod;
  * <ol>
  * <li> Request {@link ModSignal} from one source and store it somewhere<br/>
  * <li> We need to know if source was changed, just request {@link ModSignal} again from the same source<br/>
- * <li> Compare these two using {@link #equals(Object)}
+ * <li> Compare these two using {@link Object#equals(Object)}
  * </ol>
  * Two {@link ModSignal}s from two different sources must be equal if they point to the same content.
  */
