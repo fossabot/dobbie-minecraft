9b64204eee615b751c45a8fc9bfad921ba281e05
j0x0f <55847520+j0x0f@users.noreply.github.com>
2020-01-15 16:07:45 +0500

    ExecutePlayer

---
+++ minecraft/common/src/main/java/live/dobbie/minecraft/dest/cmd/ExecutePlayer.java
@@ -0,0 +1,41 @@
+package live.dobbie.minecraft.dest.cmd;
+
+import live.dobbie.core.dest.cmd.AbstractSubstitutorCmd;
+import live.dobbie.core.dest.cmd.CmdContext;
+import live.dobbie.core.dest.cmd.CmdExecutionException;
+import live.dobbie.core.dest.cmd.CmdResult;
+import live.dobbie.core.substitutor.Substitutable;
+import live.dobbie.core.substitutor.SubstitutableParser;
+import live.dobbie.core.user.User;
+import live.dobbie.minecraft.compat.MinecraftOnlinePlayer;
+import lombok.NonNull;
+
+public class ExecutePlayer extends AbstractSubstitutorCmd {
+    public ExecutePlayer(@NonNull Substitutable substitutable) {
+        super(substitutable);
+    }
+
+    @Override
+    protected CmdResult execute(@NonNull CmdContext context, @NonNull String command) throws CmdExecutionException {
+        User user = context.getUser();
+        if (user == null) {
+            throw new CmdExecutionException("context does not provider user, cannot execute as null");
+        }
+        if (!(user instanceof MinecraftOnlinePlayer)) {
+            throw new CmdExecutionException("user provided in this context is not " + MinecraftOnlinePlayer.class.getSimpleName());
+        }
+        ((MinecraftOnlinePlayer) user).executeCommand(command);
+        return CmdResult.SHOULD_CONTINUE;
+    }
+
+    public static class Parser extends AbstractSubstitutorCmd.Parser {
+        public Parser(@NonNull SubstitutableParser parser) {
+            super(parser);
+        }
+
+        @Override
+        protected AbstractSubstitutorCmd create(@NonNull Substitutable substitutable) {
+            return new ExecutePlayer(substitutable);
+        }
+    }
+}
