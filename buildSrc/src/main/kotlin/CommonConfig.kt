e53cbe34c3bb4ad9fd034925e0d145a94bce82f5
j0x0f <55847520+j0x0f@users.noreply.github.com>
2020-01-19 00:16:36 +0500

    use j0x0f snapshots maven server

--- buildSrc/src/main/kotlin/CommonConfig.kt
+++ buildSrc/src/main/kotlin/CommonConfig.kt
@@ -10,6 +10,10 @@ fun Project.applyCommonConfiguration() {
     repositories {
         mavenCentral()
         maven {
+            name = "j0x0f snapshots"
+            url = uri("https://maven.j0x0f.dev/snapshots")
+        }
+        maven {
             name = "JFrog Artifactory OSS"
             url = uri("https://oss.jfrog.org/artifactory/libs-release")
         }
