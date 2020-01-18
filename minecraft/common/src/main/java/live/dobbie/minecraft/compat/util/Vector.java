99058e4121c5a0a885d9ca22914ed3e87c089095
j0x0f <55847520+j0x0f@users.noreply.github.com>
2020-01-18 16:47:26 +0500

    more Vector methods

--- minecraft/common/src/main/java/live/dobbie/minecraft/compat/util/Vector.java
+++ minecraft/common/src/main/java/live/dobbie/minecraft/compat/util/Vector.java
@@ -1,8 +1,46 @@
 package live.dobbie.minecraft.compat.util;
 
-import lombok.Value;
+import lombok.*;
 
-@Value
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@ToString
 public class Vector {
-    double x, y, z;
+    private double x, y, z;
+
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public Vector setX(double x) {
+        this.x = x;
+        return this;
+    }
+
+    public Vector setY(double y) {
+        this.y = y;
+        return this;
+    }
+
+    public Vector setZ(double z) {
+        this.z = z;
+        return this;
+    }
+
+    public Vector multiply(@NonNull Vector vector) {
+        return setX(getX() * vector.getX()).setY(getY() * vector.getY()).setZ(getZ() * vector.getZ());
+    }
+
+    public Vector multiply(double x, double y, double z) {
+        return multiply(new Vector(x, y, z));
+    }
+
+    public Vector copy() {
+        return new Vector(getX(), getY(), getZ());
+    }
+
+    public static Vector unit() {
+        return new Vector(1., 1., 1.);
+    }
 }
