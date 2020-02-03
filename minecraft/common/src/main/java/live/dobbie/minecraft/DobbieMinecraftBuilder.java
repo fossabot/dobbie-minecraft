fb38a82df9b7fa78f42cd346bba8cb43f13e0f37
j0x0f <git@j0x0f.dev>
2020-02-03 15:57:16 +0500

    Add user-user H2 dict database * always created, not on-demand (should fix this?) * accessible through js: `persistenceService.name("db").get("foo")`

--- minecraft/common/src/main/java/live/dobbie/minecraft/DobbieMinecraftBuilder.java
+++ minecraft/common/src/main/java/live/dobbie/minecraft/DobbieMinecraftBuilder.java
@@ -29,6 +29,11 @@ import live.dobbie.core.dest.cmd.*;
 import live.dobbie.core.dest.cmd.script.AssertionScriptCmdParser;
 import live.dobbie.core.dest.cmd.script.ConditionalScriptCmdParser;
 import live.dobbie.core.dest.cmd.script.ScriptCmdParser;
+import live.dobbie.core.dictionary.sql.PlainSQLDictionaryDatabaseAdapter;
+import live.dobbie.core.dictionary.sql.SQLDictionaryDatabaseFactory;
+import live.dobbie.core.dictionary.sql.h2.H2;
+import live.dobbie.core.dictionary.sql.h2.H2DictionaryDatabaseInitializer;
+import live.dobbie.core.dictionary.sql.pool.hikari.HikariConnectionPoolFactory;
 import live.dobbie.core.loc.Loc;
 import live.dobbie.core.misc.Price;
 import live.dobbie.core.misc.currency.ICUCurrencyFormatter;
@@ -40,6 +45,7 @@ import live.dobbie.core.misc.primitive.converter.PrimitiveConverterProvider;
 import live.dobbie.core.misc.primitive.converter.SequentalConverterProvider;
 import live.dobbie.core.path.Path;
 import live.dobbie.core.persistence.PersistenceService;
+import live.dobbie.core.persistence.PrimitiveDictionaryPersistence;
 import live.dobbie.core.persistence.SessionObjectStorage;
 import live.dobbie.core.plugin.DobbiePlugin;
 import live.dobbie.core.plugin.ticker.ScheduledThreadPoolTicker;
@@ -190,7 +196,16 @@ public class DobbieMinecraftBuilder {
                     }
                 }))
                 .registerFactory(PersistenceService.class, new PersistenceService.RefFactory(Arrays.asList(
-                        new SessionObjectStorage.Factory()
+                        new SessionObjectStorage.Factory(),
+                        new PrimitiveDictionaryPersistence.FactoryDelegated(
+                                new SQLDictionaryDatabaseFactory(
+                                        user -> H2.file(new File(configDir, "db/" + user.getName())).build(),
+                                        new H2DictionaryDatabaseInitializer(),
+                                        new PlainSQLDictionaryDatabaseAdapter(),
+                                        new HikariConnectionPoolFactory()
+                                ),
+                                "sql"
+                        )
                 )))
                 .registerFactory(IdTaskScheduler.class, new IdSchedulerService.RefFactory())
                 .registerFactory(StreamLabsApi.class, new StreamLabsApi.RefFactory(userSettingsProvider));
