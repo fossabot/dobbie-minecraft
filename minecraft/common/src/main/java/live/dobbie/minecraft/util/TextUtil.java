4f81f6b3360972d74058cb02ce2a8447a9222bc7
j0x0f <git@j0x0f.dev>
2020-02-08 19:53:10 +0500

    don't use @UtilityClass because it breaks lombok FeelsSpecialMan

--- minecraft/common/src/main/java/live/dobbie/minecraft/util/TextUtil.java
+++ minecraft/common/src/main/java/live/dobbie/minecraft/util/TextUtil.java
@@ -3,11 +3,12 @@ package live.dobbie.minecraft.util;
 import live.dobbie.util.formatting.text.serializer.gson.GsonComponentSerializer;
 import live.dobbie.util.formatting.text.serializer.legacy.LegacyComponentSerializer;
 import lombok.NonNull;
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
 public class TextUtil {
     public static String toJsonText(@NonNull String legacyText) {
         return GsonComponentSerializer.INSTANCE.serialize(LegacyComponentSerializer.legacy().deserialize(legacyText));
+    }
+
+    private TextUtil() {
     }
 }
--- minecraft/forge/src/main/java/live/dobbie/minecraft/forge/compat/util/ForgeTextUtil.java
+++ minecraft/forge/src/main/java/live/dobbie/minecraft/forge/compat/util/ForgeTextUtil.java
@@ -2,16 +2,17 @@ package live.dobbie.minecraft.forge.compat.util;
 
 import live.dobbie.minecraft.util.TextUtil;
 import lombok.NonNull;
-import lombok.experimental.UtilityClass;
 import net.minecraft.util.text.ITextComponent;
 
-@UtilityClass
 public class ForgeTextUtil {
-    public ITextComponent jsonToNative(@NonNull String jsonText) {
+    public static ITextComponent jsonToNative(@NonNull String jsonText) {
         return ITextComponent.Serializer.fromJson(jsonText);
     }
 
-    public ITextComponent legacyToNative(@NonNull String legacyText) {
+    public static ITextComponent legacyToNative(@NonNull String legacyText) {
         return jsonToNative(TextUtil.toJsonText(legacyText));
+    }
+
+    private ForgeTextUtil() {
     }
 }
